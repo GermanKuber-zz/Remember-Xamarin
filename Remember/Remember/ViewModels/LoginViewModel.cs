24eb37aba1f2ebb78f5e871727dfbe5c39d77783
Mariano Germán Villarreal Küber <Mariano Germn Villarreal Kber>
2016-11-15 09:54:24 -0300

    Se agregan cambios

--- Remember/Remember/ViewModels/LoginViewModel.cs
+++ Remember/Remember/ViewModels/LoginViewModel.cs
@@ -1,24 +1,9 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
-using Remember.Model;
 using Remember.Services;
 
 namespace Remember.ViewModels
 {
-
-    public class NotificationChangedBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-
     public class LoginViewModel : NotificationChangedBase
     {
         private readonly NavigationService _navigationService;
